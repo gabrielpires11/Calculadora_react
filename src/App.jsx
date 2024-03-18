82a04f9a08ab17499ae9b6b80ae2d311c2a016f7
gabrielpires11 <gabri4590@gmail.com>
2024-03-18 17:15:10 -0300

    Gabriel

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Calculator from '/Temp/calculadora_react/src/componentes/Calculator'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Calculator />
+    </>
+  )
+}
+
+export default App
